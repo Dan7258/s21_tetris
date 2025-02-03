9e51d5777e0471ad761b27ebfab6ca387dab2fd6
dan7258 <italyevdm03@gmail.com>
2025-02-03 14:02:01 +0300

    develop исправил баг с поворотом на последней линии

--- brick_game/tetris/figure_functions.c
+++ brick_game/tetris/figure_functions.c
@@ -206,7 +206,7 @@ void s21_move_figure_all_down() {
 void s21_turn_figure() {
   condition_t *condition = s21_get_current_condition();
   s21_turn_left_matrix(condition->figure->matrix);
-  if(!s21_is_figure_valid(condition->figure)) {
+  if (!s21_is_figure_valid(condition->figure)) {
     s21_fix_figure(condition->figure);
   }
 }
@@ -215,7 +215,8 @@ void s21_add_figure_on_field() {
   condition_t *condition = s21_get_current_condition();
   for (int m = 0; m < condition->figure->matrix->rows; m++) {
     for (int n = 0; n < condition->figure->matrix->columns; n++) {
-      if (m + condition->figure->y >= 0 && m + condition->figure->y < ROWS_FIELD && 
+      if (m + condition->figure->y >= 0 &&
+          m + condition->figure->y < ROWS_FIELD &&
           condition->figure->matrix->matrix[m][n]) {
         condition->field
             ->matrix[m + condition->figure->y][n + condition->figure->x] = 1;
@@ -228,7 +229,8 @@ void s21_remove_figure_on_field() {
   condition_t *condition = s21_get_current_condition();
   for (int m = 0; m < condition->figure->matrix->rows; m++) {
     for (int n = 0; n < condition->figure->matrix->columns; n++) {
-      if (m + condition->figure->y >= 0 && m + condition->figure->y < ROWS_FIELD &&
+      if (m + condition->figure->y >= 0 &&
+          m + condition->figure->y < ROWS_FIELD &&
           condition->figure->matrix->matrix[m][n]) {
         condition->field
             ->matrix[m + condition->figure->y][n + condition->figure->x] = 0;
@@ -247,18 +249,20 @@ void s21_spawn() {
 int s21_is_figure_valid(figure_t *figure) {
   condition_t *condition = s21_get_current_condition();
   int flag = 1;
-  for(int m = figure->matrix->rows - 1; m > 0 && flag; m--) {
-    for(int n = 0; n < figure->matrix->columns && flag; n++) {
-      if((m + figure->y < 0)) {
+  for (int m = figure->matrix->rows - 1; m >= 0 && flag; m--) {
+    for (int n = 0; n < figure->matrix->columns && flag; n++) {
+      if ((m + figure->y < 0)) {
         continue;
       }
-      if(figure->matrix->matrix[m][n] && ((n + figure->x) < 0 || (n + figure->x) >= COLS_FIELD)) {
+      if (figure->matrix->matrix[m][n] &&
+          ((n + figure->x) < 0 || (n + figure->x) >= COLS_FIELD)) {
         flag = 0;
       }
-      if(flag && figure->matrix->matrix[m][n] && (m + figure->y) >= ROWS_FIELD) {
+      if ((m + figure->y) >= ROWS_FIELD) {
         flag = 0;
       }
-      if(flag && condition->field->matrix[m + figure->y][n + figure->x] && figure->matrix->matrix[m][n]) {
+      if (flag && condition->field->matrix[m + figure->y][n + figure->x] &&
+          figure->matrix->matrix[m][n]) {
         flag = 0;
       }
     }
@@ -270,24 +274,31 @@ void s21_fix_figure(figure_t *figure) {
   condition_t *condition = s21_get_current_condition();
   int pastX = figure->x;
   int pastY = figure->y;
-  for(int m = figure->matrix->rows - 1; m > 0; m--) {
-    for(int n = 0; n < figure->matrix->columns; n++) {
-      if(figure->matrix->matrix[m][n] && (m + figure->y) >= ROWS_FIELD) {
-        figure->y -= ((m + figure->y) - (ROWS_FIELD - 1));
-      } else if(figure->matrix->matrix[m][n] && (n + figure->x) < 0) {
+  for (int m = figure->matrix->rows - 1; m >= 0; m--) {
+    for (int n = 0; n < figure->matrix->columns; n++) {
+      if (figure->matrix->matrix[m][n] && (n + figure->x) < 0) {
         figure->x += 0 - (n + figure->x);
-      } else if(figure->matrix->matrix[m][n] && (n + figure->x) >= COLS_FIELD) {
+      } else if (figure->matrix->matrix[m][n] &&
+                 (n + figure->x) >= COLS_FIELD) {
         figure->x -= (n + figure->x) - (COLS_FIELD - 1);
-      } else if(figure->matrix->matrix[m][n] && condition->field->matrix[m + figure->y][n + figure->x] && m <= figure->matrix->columns / 2) {
-        for(;figure->matrix->matrix[m][n] && condition->field->matrix[m + figure->y][n + figure->x]; figure->x++) {
+      } else if ((m + figure->y) < ROWS_FIELD && figure->matrix->matrix[m][n] &&
+                 condition->field->matrix[m + figure->y][n + figure->x] &&
+                 n <= figure->matrix->columns / 2) {
+        for (; figure->matrix->matrix[m][n] &&
+               condition->field->matrix[m + figure->y][n + figure->x];
+             figure->x++) {
         }
-      } else if(figure->matrix->matrix[m][n] && condition->field->matrix[m + figure->y][n + figure->x] && m > figure->matrix->columns / 2) {
-        for(;figure->matrix->matrix[m][n] && condition->field->matrix[m + figure->y][n + figure->x]; figure->x--) {
+      } else if ((m + figure->y) < ROWS_FIELD && figure->matrix->matrix[m][n] &&
+                 condition->field->matrix[m + figure->y][n + figure->x] &&
+                 n > figure->matrix->columns / 2) {
+        for (; figure->matrix->matrix[m][n] &&
+               condition->field->matrix[m + figure->y][n + figure->x];
+             figure->x--) {
         }
       }
     }
   }
-  if(!s21_is_figure_valid(figure)) {
+  if (!s21_is_figure_valid(figure)) {
     figure->x = pastX;
     figure->y = pastY;
     s21_turn_right_matrix(figure->matrix);
